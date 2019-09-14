0bc15ff148b8e1ac50f4f3f4c416ce12b3ebe78c
Idrees Dargahwala <theiyd@Idreess-MacBook-Pro.local>
2019-09-15 01:33:58 +0530

    Store memes on database

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
-require("./lib/cron");
+require("dotenv").config();
+
+// Connect mongoose
+mongoose
+  .connect(process.env.MONGODB)
+  .then(db => {
+    console.log("MongoDB connected");
+    require("./lib/cron");
+  })
+  .catch(err => {
+    console.error(err);
+  });
 
 app.listen(7000, () => {
   console.log("Server started on port 7000");
