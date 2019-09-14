4d630398cb950dbdf2df01e397941131290cd243
Idrees <dargahidrees@gmail.com>
2019-09-15 02:08:40 +0530

    Add keyword route

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const addKeyword = require("./lib/utility");
 const app = express();
 
 require("dotenv").config();
@@ -19,4 +20,16 @@ mongoose
 let port = process.env.PORT || 7000;
 app.listen(port, () => {
   console.log("Server started on port " + port);
+});
+
+// Add keywords
+app.post("/keyword", async (req, res, next) => {
+  const id = req.query.id;
+  const keyword = req.query.keyword;
+  if (keyword) {
+    const saveKeyword = await addKeyword(id, keyword);
+    if (saveKeyword) {
+      res.status(200).json({ message: "Keyword added" });
+    }
+  }
 });
